ddba43158457f10f5953e9ffc133c7a31ebc2753
hollowdll <pixerchat@gmail.com>
2022-12-25 16:03:40 +0200

    practice match

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code)]
 
+// Practice basics
+// Uncomment if needed
+/*
 #[derive(Debug)]
 enum Color {
     Red(String),
@@ -34,4 +37,57 @@ fn main() {
 
     let message = Message::Write(String::from("Hello world!"));
     message.call();
+}
+*/
+
+
+// Practice match
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    /*
+    let penny = Coin::Penny;
+    println!("{}", value_in_cents(penny));
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+    */
+
+    // Option enum
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _none = plus_one(None);
+    println!("{:?}", five);
 }
