14c28f45df669d90d1538cd62eee4bd3d6f41d75
hollowdll <pixerchat@gmail.com>
2022-12-30 21:29:49 +0200

    practice error handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,11 +1,37 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let file_result = File::open("hello.txt");
 
     let _text_file = match file_result {
         Ok(file) => file,
-        // Panic if not found
-        Err(error) => panic!("Problem opening the file: {}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(created_file) => created_file,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
     };
+
+    // Same but another way with closures
+    /*
+    let text_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+    */
+
+    // Shortcut methods on Result<T, E> for panic
+    let _file_result = File::open("hello.txt").unwrap();
+    let _file_result = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
 }
