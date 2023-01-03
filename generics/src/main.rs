81545ead163c5f65b8b06d86f9ad6e5ea4ae225b
hollowdll <pixerchat@gmail.com>
2023-01-03 13:10:03 +0200

    practice generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -2,7 +2,7 @@
 
 // Avoid code duplication
 // No generics
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
 
     for item in list {
@@ -14,15 +14,107 @@ fn largest(list: &[i32]) -> &i32 {
     largest
 }
 
-// Call without generics
+// No generics
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// Generic version of above functions
+// Accepts types that can be compared
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// Call with generic function
+/*
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['a', 'b', 'c'];
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
+*/
+
+// Generic struct
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// Only on instances with f64 type
+impl Point<f64, f64> {
+    fn math_time(&self) -> f64 {
+        self.x.powi(2) + self.y.powi(3)
+    }
+}
+
+/*
+fn main() {
+    let integer = Point { x: 10, y: -5 };
+    let float = Point { x: 2.0, y: 6.0 };
+    let integer_and_float = Point { x: 8, y: 4.0 };
+}
+*/
+
+// Option and Result enums use generics
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+// Generic type parameters can
+// be different in methods
+struct Point2<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point2<X1, Y1> {
+    // other might have different generic types than self
+    fn mixup<X2, Y2>(self, other: Point2<X2, Y2>) -> Point2<X1, Y2> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = Point2 { x: 6, y: 12.7 };
+    let p2 = Point2 { x: "Hi", y: 'f' };
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
