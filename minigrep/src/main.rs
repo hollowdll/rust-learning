cdb4cdf1a76f4b73115c39246129790fd5d4b4e6
hollowdll <pixerchat@gmail.com>
2023-01-15 20:26:58 +0200

    progress on command line tool

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,43 +6,26 @@
 
 use std::{
     env,
-    fs,
+    process,
 };
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        // If not enough arguments
-        if args.len() < 3 {
-            panic!(
-                "Error: Not enough arguments! \
-                [query] and [file_path] required!"
-            );
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
-}
+use minigrep::Config;
 
 fn main() {
     // Collect arguments passed into the program
     let args: Vec<String> = env::args().collect();
     
     // Ignore first argument, which is the program
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Error reading the file!");
-
-    println!("With text:\n\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
