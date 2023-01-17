16e839ef213b1e101dd00764570b0df2b375d081
hollowdll <pixerchat@gmail.com>
2023-01-17 21:08:52 +0200

    write errors to stderr when redirecting output to a file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -19,10 +19,12 @@ fn main() {
     
     // Ignore first argument, which is the program
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
