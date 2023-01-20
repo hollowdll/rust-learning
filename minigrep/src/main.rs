1f0d2a7d10e0bcd5057f1163d0e58001f155d606
hollowdll <pixerchat@gmail.com>
2023-01-20 19:42:31 +0200

    improve code with iterators

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -14,11 +14,8 @@ use std::{
 use minigrep::Config;
 
 fn main() {
-    // Collect arguments passed into the program
-    let args: Vec<String> = env::args().collect();
-    
     // Ignore first argument, which is the program
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
