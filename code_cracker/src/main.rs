fa4f22753ee395b2d5c783af264b2721121fbeb5
hollowdll <pixerchat@gmail.com>
2023-01-30 20:21:04 +0200

    generate six digit text code

--- code_cracker/src/main.rs
+++ code_cracker/src/main.rs
@@ -14,14 +14,42 @@ use rand::{
 use std::time::Instant;
 use std::io;
 
-fn generate_rand_code(rng: &mut ThreadRng) -> u32 {
+fn gen_code(rng: &mut ThreadRng) -> u32 {
     println!("Generating random code...");
 
-    let random_code = rng.gen_range(1..=1_000_000);
+    let random_code = rng.gen_range(0..=1_000_000);
 
     println!("Random code: {}", random_code);
 
     random_code
+}
+
+fn gen_six_digit_code(rng: &mut ThreadRng) -> String {
+    println!("Generating random six digit code...");
+
+    // code format is "000 000"
+
+    let code = rng.gen_range(0..=999_999);
+    let mut final_code: String;
+
+    final_code = match code {
+        0..=9 => format!("00000{code}"),
+        10..=99 => format!("0000{code}"),
+        100..=990 => format!("000{code}"),
+        1000..=9999 => format!("00{code}"),
+        10000..=99999 => format!("0{code}"),
+        100000..=999999 => format!("{code}"),
+        _ => {
+            eprintln!("Invalid six digit code!");
+            String::from("000000")
+        }
+    };
+
+    final_code.insert(3, ' ');
+
+    println!("{final_code}");
+
+    final_code
 }
 
 fn crack_with_range(code: &u32) {
@@ -73,17 +101,17 @@ fn crack_with_loop(code: &u32) {
     println!("Nanosecond: {}", elapsed.as_nanos());
 }
 
-fn main() {
+fn init_with_number_code() {
     let mut rng = rand::thread_rng();
-    let code = generate_rand_code(&mut rng);
-
+    let code = gen_code(&mut rng);
+    
     println!("Choose cracking method by typing its number:");
     println!("1. Range");
     println!("2. Loop");
     println!("3. Range and loop");
-
+    
     let mut option = String::new();
-
+    
     io::stdin()
         .read_line(&mut option)
         .expect("Failed to read line!");
@@ -107,4 +135,17 @@ fn main() {
         },
         _ => panic!("Invalid input!"),
     }
+}
+
+fn init_with_text_code() {
+    let mut rng = rand::thread_rng();
+    let code = gen_six_digit_code(&mut rng);
+
+    // Todo
+    // Loop every possible value until
+    // code is cracked
+}
+
+fn main() {
+    init_with_text_code();
 }
