91ef4f63ad8928d826a452d3c27390f3ae4b5eac
hollowdll <pixerchat@gmail.com>
2023-02-04 21:01:36 +0200

    move data into thread + message passing

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -6,14 +6,16 @@
 // Writing multi-threaded programs requires
 // extra attention and details!
 // Even a small mistake can introduce bugs
-// and possibly vulnerabilities!
+// in complex contexts.
+// These examples are simple though.
 
 #![allow(unused)]
 
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
-fn main() {
+fn thread_example() {
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("Hello number {i} from the spawned thread!");
@@ -27,4 +29,32 @@ fn main() {
     }
     
     handle.join().unwrap();
+}
+
+fn move_data() {
+    let nums = vec![1,2,3,4,5];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector in spawned thread: {:?}", nums);
+    });
+
+    handle.join().unwrap();
+}
+
+fn message_passing() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let message = String::from("Yo!");
+        tx.send(message).unwrap();
+    });
+
+    let received_message = rx.recv().unwrap();
+    println!("Received message: {}", received_message);
+}
+
+fn main() {
+    // thread_example();
+    // move_data();
+    message_passing();
 }
