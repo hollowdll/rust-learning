612325bd4f639d2ba6f74512c2dfcdf4fc4c98e9
hollowdll <pixerchat@gmail.com>
2023-02-05 19:47:03 +0200

    multiple messages and threads

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -41,7 +41,7 @@ fn move_data() {
     handle.join().unwrap();
 }
 
-fn message_passing() {
+fn simple_message_passing() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
@@ -53,8 +53,83 @@ fn message_passing() {
     println!("Received message: {}", received_message);
 }
 
+// Send multiple messages via one transmitter
+fn multiple_messages() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let messages = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("spawned"),
+            String::from("thread!"),
+        ];
+
+        for message in messages {
+            tx.send(message).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    // Iterate over received messages
+    for received in rx {
+        // thread::sleep(Duration::from_secs(1));
+        println!("Got {}", received);
+    }
+}
+
+// Send messages from multiple threads
+// to one receiver
+fn multiple_threads() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let messages = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("spawned"),
+            String::from("thread!"),
+        ];
+
+        for message in messages {
+            tx1.send(message).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    thread::spawn(move || {
+        let messages = vec![
+            String::from("Yo"),
+            String::from("from"),
+            String::from("the"),
+            String::from("other"),
+            String::from("spawned"),
+            String::from("thread!"),
+            String::from("that"),
+            String::from("has"),
+            String::from("a longer"),
+            String::from("message"),
+        ];
+
+        for message in messages {
+            tx.send(message).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    for received in rx {
+        // thread::sleep(Duration::from_secs(1));
+        println!("Got {}", received);
+    }
+}
+
 fn main() {
     // thread_example();
     // move_data();
-    message_passing();
+    // simple_message_passing();
+    // multiple_messages();
+    multiple_threads();
 }
