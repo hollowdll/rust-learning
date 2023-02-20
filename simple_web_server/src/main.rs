f5a6d6965ae07e0838efff9dd4a6d399b3c5c5ff
hollowdll <pixerchat@gmail.com>
2023-02-20 11:30:25 +0200

    init thread pool model

--- simple_web_server/src/main.rs
+++ simple_web_server/src/main.rs
@@ -1,20 +1,27 @@
-// Simple single-threaded web server that runs on
-// local address 127.0.0.1:7878 (localhost port 7878)
+// Web server runs on 127.0.0.1:7878 (localhost port 7878)
+// Uses thread pool model
+// WIP: Multithreaded model (not finished)
 
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use simple_web_server::ThreadPool;
 
 fn main() {
     let server_address = "127.0.0.1:7878";
-    let listener = TcpListener::bind(&server_address).unwrap();
+    let listener = TcpListener::bind(server_address).unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -22,10 +29,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
