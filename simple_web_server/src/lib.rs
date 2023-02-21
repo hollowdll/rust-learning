8ba5c97209a15030abf04996e5a7c81ce402003d
hollowdll <pixerchat@gmail.com>
2023-02-21 13:10:03 +0200

    threads will finish executing before shutting down

--- simple_web_server/src/lib.rs
+++ simple_web_server/src/lib.rs
@@ -12,7 +12,7 @@ use std::{
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -40,23 +40,27 @@ impl ThreadPool {
 
         Self {
             workers,
-            sender,
+            sender: Some(sender),
         }
     }
 
     /// Execute a task in one of the thread pool's threads
+    /// 
+    /// f is a closure that will be run in a different thread
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        drop(self.sender.take());
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -74,14 +78,22 @@ struct Worker {
 
 impl Worker {
     // id is the id of the worker
-    // receiver receives sent tasks.
+    // receiver receives sent messages.
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job; executing");
-
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing");
+                    
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
 
         Self {
